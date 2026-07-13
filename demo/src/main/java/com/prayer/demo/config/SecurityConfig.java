dd3db3fcb53b8ed78c576da1589970aa6f7c1e24
Zakongjampa <Zakongjampa@gmail.com>
2026-07-13 22:11:25 +0200

    new column named is_admin

--- demo/src/main/java/com/prayer/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/prayer/demo/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
                 "http://localhost:5175",
+                "https://prayer-frontend-eight.vercel.app",
                 "http://127.0.0.1:5175"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
